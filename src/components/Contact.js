e54ec82a8b042a3e705a43cac1b03144335b5575
hugo jenouvrier <hugo35340@gmail.com>
2022-03-23 20:36:33 +0100

    Add list contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 
-class Contact extends Component {
-
-    render() {
-        return (
-                <div className='card card-body mb-3'>
-                    <h4>{this.props.nom}</h4>
-                    <ul className='list-group'>
-                        <li className='list-group-item'>
-                            Email : {this.props.email}
-                        </li>
-                        <li className='list-group-item'>
-                            Téléphone : {this.props.tel}
-                        </li>
-                    </ul>
-                </div>
-        );
+function Contact({contact}) {
+    return (
+        <div className='card card-body mb-3'>
+            <h4>{contact.nom}</h4>
+            <ul className='list-group'>
+                <li className='list-group-item'>
+                    Email : {contact.email}
+                </li>
+                <li className='list-group-item'>
+                    Téléphone : {contact.tel}
+                </li>
+            </ul>
+        </div>
+);
     }
-}
+
 
 export default Contact;
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,42 +1,82 @@
 import React, { Component } from 'react';
-import Contact from './Contact';
 
 class Contacts extends Component {
 
     state = {
-        contacts: [
-            {
-                id: 1,
-                nom: 'Jack Dupont',
-                email: 'jack.dupont@mail.com',
-                tel: '01-02-03-04-05'
-            },
-            {
-                id: 2,
-                nom: 'Jean Fontaine',
-                email: 'jean.fontaine@mail.com',
-                tel: '01-22-33-44-55'
-            },
-            {
-                id: 3,
-                nom: 'Pierre Lamarre',
-                email: 'pierre.lamarre@mail.com',
-                tel: '01-23-45-56-78'
-            }
-        ]
-    } 
+        nom: '',
+        email: '',
+        tel: '',
+        items: []
+    }
+
+    onChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+        // console.log(this.state.nom);
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            nom: '',
+            email: '',
+            tel: '',
+            items: [...this.state.items, { nom: this.state.nom, email: this.state.email, tel: this.state.tel }]
+        });
+    }
+
+    renderContact = () => {
+        return this.state.items.map((item, index) => {
+            return (
+                <div className='card card-body mb-3' key={index}>
+                    <h4>{item.nom}</h4>
+                    <ul className='list-group'>
+                        <li className='list-group-item'>
+                            Email : {item.email}
+                        </li>
+                        <li className='list-group-item'>
+                            Téléphone : {item.tel}
+                        </li>
+                    </ul>
+                </div>
+            )
+        })
+    }
 
     render() {
         return (
-            <div>
-                {this.state.contacts.map(contact =>(
-                    <Contact 
-                        key={contact.id}
-                        nom={contact.nom}
-                        email={contact.email}
-                        tel={contact.tel}
-                    />
-                ))}
+            <div class="row gx-5">
+                <div class="col">
+                    <div class="p-3 border bg-light">
+                        {this.renderContact()}
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="p-3 border bg-light">
+                        <form onSubmit={this.onSubmit}>
+                            <div className='form-group'>
+                                <label htmlFor='nom'>Nom et prénom</label>
+                                <input type='text' className='form-control' name='nom'
+                                    onChange={this.onChange} value={this.state.nom} required></input>
+                            </div>
+
+                            <div className='form-group'>
+                                <label htmlFor='email'>email</label>
+                                <input type='text' className='form-control' name='email'
+                                    onChange={this.onChange} value={this.state.email} required></input>
+                            </div>
+
+                            <div className='form-group'>
+                                <label htmlFor='tel'>Téléphone</label>
+                                <input type='text' className='form-control' name='tel'
+                                    onChange={this.onChange} value={this.state.tel} required></input>
+                            </div>
+
+                            <button className='btn btn-primary btn-block'>Ajouter Contact</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }
